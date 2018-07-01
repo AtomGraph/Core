bdff7ab266b2c703e5a3724e2c79cc33ce833b62
Martynas <martynas@graphity.org>
2018-07-01 18:08:05 +0200

    JavaDoc fix

--- src/main/java/com/atomgraph/core/provider/ClientProvider.java
+++ src/main/java/com/atomgraph/core/provider/ClientProvider.java
@@ -32,7 +32,7 @@ import org.slf4j.LoggerFactory;
  * Needs to be registered in the JAX-RS application.
  * 
  * @author Martynas Jusevičius {@literal <martynas@atomgraph.com>}
- * @see import com.sun.jersey.api.client.Client
+ * @see com.sun.jersey.api.client.Client
  * @see javax.ws.rs.core.Context
  */
 @Provider
