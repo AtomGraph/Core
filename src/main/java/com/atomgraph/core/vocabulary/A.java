6d0f730b6c255b0d8fb20e58f34088611fbe9794
Martynas <martynas@graphity.org>
2018-12-01 11:04:08 +0100

    Added A.statusCode property

--- src/main/java/com/atomgraph/core/vocabulary/A.java
+++ src/main/java/com/atomgraph/core/vocabulary/A.java
@@ -61,7 +61,9 @@ public final class A
     public static final ObjectProperty requestUri = m_model.createObjectProperty( NS + "requestUri" );
 
     public static final DatatypeProperty method = m_model.createDatatypeProperty( NS + "method" );
-    
+
+    public static final DatatypeProperty statusCode = m_model.createDatatypeProperty( NS + "statusCode" );
+
     public static final DatatypeProperty httpHeaders = m_model.createDatatypeProperty( NS + "httpHeaders" );
 
     public static final DatatypeProperty maxGetRequestSize = m_model.createDatatypeProperty( NS + "maxGetRequestSize" );
