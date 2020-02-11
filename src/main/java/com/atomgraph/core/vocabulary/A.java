c93d39387752352b4bded9daeb36c756d8e8a4d2
Martynas <martynas@graphity.org>
2020-02-11 11:58:12 +0100

    Fixed namespace URI in Java code

--- src/main/java/com/atomgraph/core/vocabulary/A.java
+++ src/main/java/com/atomgraph/core/vocabulary/A.java
@@ -33,7 +33,7 @@ public final class A
     private static OntModel m_model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
     
     /** <p>The namespace of the vocabulary as a string</p> */
-    public static final String NS = "http://atomgraph.com/ns/core#";
+    public static final String NS = "https://w3id.org/atomgraph/core#";
     
     /** <p>The namespace of the vocabulary as a string</p>
      * @return namespace URI
